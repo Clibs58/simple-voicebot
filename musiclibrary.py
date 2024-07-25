7a59e9b0c9e027be452972ba2eae98ed68a0fbae
Abhishek Rana <mailtoabhishekrana@gmail.com>
2024-07-25 15:54:58 +0530

    updated

---
+++ musiclibrary.py
@@ -0,0 +1,12 @@
+music={
+    "rex":"https://www.youtube.com/watch?v=SBfoWTB-T_U",
+    "heat":"https://www.youtube.com/watch?v=mRD0-GxqHVo",
+    "seasons":"https://www.youtube.com/watch?v=9tIBfNZK0Q8"
+}
+
+musicS={
+    "dandelions":"https://open.spotify.com/track/2eAvDnpXP5W0cVtiI0PUxV",
+    "fontaine":"https://open.spotify.com/track/7gEpP0zIY3lVwTl7I5GZmH",
+    "darkside":"https://open.spotify.com/track/6xlOnm5MITIKtixzJW8R0j"
+    
+}
